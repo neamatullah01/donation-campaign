f9305404f7a8223b90bc6ac26f510406bcc30cd3
neamatullah01 <neamat22205101908@diu.edu.bd>
2023-09-27 19:03:43 +0600

    Set item in local storage

--- src/Components/Details/CardDetail/CardDetail.jsx
+++ src/Components/Details/CardDetail/CardDetail.jsx
@@ -1,15 +1,44 @@
+import swal from "sweetalert";
 
 
-const CardDetail = ({detail}) => {
+const CardDetail = ({ detail }) => {
 
-    const {picture, title, category, category_bg_color, card_bg_color, text_button_bg_color, description, price } = detail;
+    const { id, picture, title, text_button_bg_color, description, price } = detail;
+
+    const handleAddToDonation = () => {
+
+        const addedDonationArray = [];
+
+        const donationItems = JSON.parse(localStorage.getItem('donation'))
+
+        if (!donationItems) {
+            addedDonationArray.push(detail)
+            localStorage.setItem('donation', JSON.stringify(addedDonationArray))
+            swal("Thank You!", "Your donation added!", "success");
+        }
+        else {
+
+            const isExists = donationItems.find(donation => donation.id === id)
+
+            if (!isExists) {
+                addedDonationArray.push(...donationItems, detail)
+                localStorage.setItem('donation', JSON.stringify(addedDonationArray))
+                swal("Thank You!", "Your donation added!", "success");
+            }
+            else{
+                swal("Error!", "This donation already added!", "error");
+            }
+
+        }
+    }
 
     return (
         <div>
-            <div className="h-[60vh] relative">
+            <div className="h-[65vh] relative">
                 <img className="w-full h-full" src={picture} alt="" />
                 <div className="absolute bg-[#0000004D] w-full bottom-0 py-8 px-6">
-                    <button style={{backgroundColor: text_button_bg_color}} className="py-4 px-3 rounded-lg border-none text-xl font-semibold text-white">Donate ${price}</button>
+                    <button onClick={handleAddToDonation}
+                        style={{ backgroundColor: text_button_bg_color }} className="py-4 px-3 rounded-lg border-none text-xl font-semibold text-white">Donate ${price}</button>
                 </div>
                 <div className="py-8">
                     <h1 className="text-4xl font-bold py-8">{title}</h1>
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Donation = () => {
+
+
+    const [donations, setdonations] = useState([])
+
+    useEffect(()=>{
+
+        const donationItems = JSON.parse(localStorage.getItem('donation'));
+        setdonations(donationItems)
+
+    },[])
+
+    console.log()
+
+
     return (
         <div>
             donation
