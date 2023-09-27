b59efbd237f4ecc0a5c9a6c59acf2494b1ae3733
neamatullah01 <neamat22205101908@diu.edu.bd>
2023-09-27 17:03:25 +0600

    Added card details and donate button

--- src/Components/Details/CardDetail/CardDetail.jsx
+++ src/Components/Details/CardDetail/CardDetail.jsx
@@ -6,9 +6,15 @@ const CardDetail = ({detail}) => {
 
     return (
         <div>
-            <div className="h-[60vh]">
+            <div className="h-[60vh] relative">
                 <img className="w-full h-full" src={picture} alt="" />
-                <button>Donate {price}</button>
+                <div className="absolute bg-[#0000004D] w-full bottom-0 py-8 px-6">
+                    <button style={{backgroundColor: text_button_bg_color}} className="py-4 px-3 rounded-lg border-none text-xl font-semibold text-white">Donate ${price}</button>
+                </div>
+                <div className="py-8">
+                    <h1 className="text-4xl font-bold py-8">{title}</h1>
+                    <p className="text-base font-normal">{description}</p>
+                </div>
             </div>
         </div>
     );
